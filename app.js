bf3e49383ddc0d428a3d84e6230cbcc704900817
Rica Zhang <ricagz@yahoo.com>
2016-03-09 01:19:53 -0500

    Elephant paths from data and selection

--- app.js
+++ app.js
@@ -1,60 +1,135 @@
+'use strict';
+
 var map;
+var elephantNames = [];
+var lines = {};
+var elephants = {};
+
+/* color generator source: http://stackoverflow.com/questions/1484506/random-color-generator-in-javascript */
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+/* sort by attribute source http://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value-in-javascript */
+function compare(a,b) {
+  if (a["timestamp"] < b["timestamp"])
+    return -1;
+  else if (a.last_nom > b.last_nom)
+    return 1;
+  else 
+    return 0;
+}
+
+//objs.sort(compare);
+
+function loadElephantData() {
+    $.getJSON("locations.json")
+        .then(function(json) {
+            $.each(json, function(index, jsonObject) {
+                var name = jsonObject["name"];
+                if (elephantNames.indexOf(name) < 0) {
+                    elephantNames.push(name);
+                    elephants[name] = new Array();
+                }
+                var eleObject = {
+                    "timestamp": new Date(jsonObject["timestamp"]),
+                    "lat": parseFloat(jsonObject["x"]),
+                    "lng": parseFloat(jsonObject["y"])
+                };
+                elephants[name].push(eleObject);
+            });
+            for (var i in elephantNames) {
+            	$('<div>').attr({
+            		id: elephantNames[i],
+            	}).appendTo('#elephant-names');
+
+            	$('<input>').attr({
+				    type: 'checkbox',
+				    id: elephantNames[i] + "-input",
+				    name: elephantNames[i],
+				    class: "elephant-name-checkbox",
+				    checked: true
+				}).appendTo('#' + elephantNames[i]);
+
+				$('#' + elephantNames[i] + "-input").click(function() {
+					if($(this).is(":not(:checked)")) {
+				        lines[this.name].setMap(null);
+				    }
+				    else {
+				    	lines[this.name].setMap(map);
+				    }
+				})
+
+				var label = $("<label>").text(elephantNames[i]);
+				label.attr({
+				    for: elephantNames[i] + "-input",
+			    	id: elephantNames[i] + "-label",
+			    	text: elephantNames[i]
+				}).appendTo('#' + elephantNames[i]);
+            }
+        })
+        .fail(function( jqxhr, textStatus, error ) {
+            var err = textStatus + ", " + error;
+            console.log( "Request Failed: " + err );
+        });
+}
+
+loadElephantData();
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -0.705369, lng: 11.515727},
-    zoom: 8,
-    mapTypeId: google.maps.MapTypeId.TERRAIN
-  });
-
-  google.maps.event.addListenerOnce(map, 'tilesloaded', function(){
-    // do something only the first time the map is loaded
-    var marker = new google.maps.Marker({
-      position: {lat: -0.705369, lng: 11.515727},
-      map: map,
-      title: 'David'
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -0.5170211, lng: 9.525267},
+        zoom: 11,
+        mapTypeId: google.maps.MapTypeId.TERRAIN
     });
 
-    var lineSymbol = {
-      path: google.maps.SymbolPath.CIRCLE,
-      scale: 8,
-      strokeColor: '#393'
-    };
-
-    var elephantPath = [
-      {lat: -0.705369, lng: 11.515727},
-      {lat: -0.275891, lng: 11.455079},
-      {lat: 0.415270, lng: 12.668468},
-      {lat: -0.092899, lng: 12.535864},
-    ];
-
-    var elephantLine = new google.maps.Polyline({
-      path: elephantPath,
-      geodesic: true,
-      icons: [{
-        icon: lineSymbol,
-        offset: '100%'
-      }],
-      strokeColor: '#FF0000',
-      strokeOpacity: 1.0,
-      strokeWeight: 2
+    google.maps.event.addListenerOnce(map, 'tilesloaded', function(){
+        // do something only the first time the map is loaded
+        for (var eleName in elephants) {
+            var line = new google.maps.Polyline({
+                path: elephants[eleName],
+                geodesic: true,
+                strokeColor: getRandomColor(),
+                strokeOpacity: 1.0,
+                strokeWeight: 2
+            })
+            lines[eleName] = line;
+            line.setMap(map);
+        }
     });
-
-    elephantLine.setMap(map);
-    animateCircle(elephantLine);
-  });
 
 }
-
-
 
 function animateCircle(line) {
-  var count = 0;
-  window.setInterval(function() {
-    count = (count + 1) % 200;
+    var count = 0;
+    window.setInterval(function() {
+        count = (count + 1) % 200;
 
-    var icons = line.get('icons');
-    icons[0].offset = (count / 2) + '%';
-    line.set('icons', icons);
+        var icons = line.get('icons');
+        icons[0].offset = (count / 2) + '%';
+        line.set('icons', icons);
     }, 20);
 }
+$(function() {
+	$('#select-all-elephants').click(function(e) {
+		e.preventDefault();
+		$('.elephant-name-checkbox').each(function() {
+			if($(this).is(":not(:checked)")) {
+				$(this).trigger("click");
+		    }
+		})
+	});
+	$('#deselect-all-elephants').click(function(e) {
+		e.preventDefault();
+		$('.elephant-name-checkbox').each(function() {
+			if($(this).is(":checked")) {
+				$(this).trigger("click");
+		    }
+		})
+	})
+})
