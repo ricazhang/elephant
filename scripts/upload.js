8c7f40cb39c72638d9d16b8184a0d95752943ea6
Rica Zhang <ricagz@yahoo.com>
2016-04-26 02:18:49 -0400

    Basic CSV reading works

--- scripts/upload.js
+++ scripts/upload.js
@@ -1,27 +1,69 @@
-function readBlob() {
-
-    var files = document.getElementById('files').files;
-    if (!files.length) {
-        alert('Please select a file!');
-        return;
+function browserSupportFileUpload() {
+    var isCompatible = false;
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        isCompatible = true;
     }
-
-    var file = files[0];
-
-    var reader = new FileReader();
-
-    // If we use onloadend, we need to check the readyState.
-    reader.onloadend = function(evt) {
-        if (evt.target.readyState == FileReader.DONE) { // DONE == 2
-            document.getElementById('byte_content').textContent = evt.target.result;
-        };
-
-        reader.readAsBinaryString(file);
-    }
+    return isCompatible;
 }
-  
-document.querySelector('.readBytesButtons').addEventListener('click', function(evt) {
-    if (evt.target.tagName.toLowerCase() == 'button') {
-        readBlob();
+
+function processData(allText) {
+    var recordNum = 7;  // or however many elements there are in each row
+    var allTextLines = allText.split(/\r\n|\n/);
+    var entries = allTextLines[0].split(',');
+    var lines = [];
+    
+    var headings = entries.splice(0, recordNum);
+    while (entries.length > 0) {
+        var tarr = [];
+        var delim = " : ";
+        for (var j = 0; j < recordNum; j++) {
+            tarr.push(headings[j] + delim + entries.shift());
+        }
+        lines.push(tarr);
     }
-}, false);
+    
+    for (var i = 0; i < allTextLines.length; i++) {
+        lines.push(allTextLines[i].split(','));
+    }
+    
+    return lines;
+    // alert(lines);
+}
+
+
+$(document).ready(function() {
+    function upload(evt) {
+        if (!browserSupportFileUpload()) {
+            $('#message').html('The File APIs are not fully supported in this browser!');
+        } 
+        
+        else {
+            var data = null;
+            var file = evt.target.files[0];
+            var reader = new FileReader();
+            reader.readAsText(file);
+            reader.onloadend = function(event) {
+                var csvData = event.target.result;
+                data = processData(csvData);
+                //console.log(csvData);
+                //data = $.csv.toArrays(csvData);
+                if (data && data.length > 0) {
+                    $('#message').html('Imported -' + data.length + '- rows successfully!');
+                    
+                    for (var i = 0; i < data.length; i++) {
+                        $('#byte_content').append(data[i]);
+                        $('#byte_content').append('<br>');
+                    }
+                } else {
+                    $('#message').html('No data to import!');
+                }
+            };
+            reader.onerror = function() {
+                $('#message').html('Unable to read ' + file.fileName);
+            };
+        }
+    }
+    
+    document.getElementById('read-file-btn').addEventListener('change', upload, false);
+
+}); 
